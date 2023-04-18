f39cf4761b298cca0060c9cf8420eb9bf76b5137
nuklotron <vas.ledkov@gmail.com>
2023-04-18 23:11:06 +0300

    class Item and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def some_item():
+    return Item("Alcatel", 1000, 5)
+
+
+def test_calculate_total_price(some_item):
+    assert some_item.calculate_total_price() == 5000
+
+
+def test_apply_discount(some_item):
+    assert some_item.apply_discount() == 1000.0
