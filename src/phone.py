4e2fff5c828e6658e3ac4054c61825a6ab5c014e
nuklotron <vas.ledkov@gmail.com>
2023-05-09 12:52:15 +0300

    hw4

--- src/phone.py
+++ src/phone.py
@@ -9,20 +9,21 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
+
+        if number_of_sim > 0 and number_of_sim == int(number_of_sim):
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     @property
     def number_of_sim(self):
-        if self.__number_of_sim > 0:
-            return self.__number_of_sim
-        else:
-            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        return self.__number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, number_of_sim):
+    def number_of_sim(self, number_of_sim: int):
         if number_of_sim > 0 and number_of_sim == int(number_of_sim):
             self.__number_of_sim = number_of_sim
         else:
@@ -31,6 +32,5 @@ class Phone(Item):
     def __add__(self, other):
         if not isinstance(other, Item):
             raise ValueError("нельзя складывать Phone с другими типами кроме себя и Item")
-
         else:
             return self.quantity + other.quantity
